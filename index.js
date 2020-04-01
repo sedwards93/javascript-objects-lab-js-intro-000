e5326c9bee5d51de2fec25d91d27760f55bf0712
Symion Edwards <symion.edwards@gmail.com>
2020-04-01 14:24:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ function updateObjectWithKeyAndValue (object, key, value) {
 function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
   return Object.assign(object, {[key]:value})
 }
+
+function deleteFromObjectByKey (object, key) {
+  var = newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj
+}
